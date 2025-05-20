9073735858f28f9b214ad45f564751cdc5892027
locnht <locnht.it@gmail.com>
2025-05-20 09:09:37 +0700

    Get familiar with Java with HelloJava project

---
+++ README.md
@@ -0,0 +1,3 @@
+﻿# Object-Oriented Programming with Java
+
+#### © Copyright belongs to Loc Ngo-Huynh-Tan | [fb/locngohuynhtan](https://www.facebook.com/locngohuynhtan/) |
---
+++ Session01-Hello/HelloJava/src/hellojava/HelloJava.java
@@ -0,0 +1,13 @@
+package hellojava;
+
+public class HelloJava {
+
+    public static void main(String[] args) {
+        System.out.println("Xin chào Java. \nWelcome to Java World!");
+        
+        System.out.println("5 + 5 = " + 5 + 5);
+        
+        System.out.println("5 + 5 = " + (5 + 5));
+    }
+    
+}
