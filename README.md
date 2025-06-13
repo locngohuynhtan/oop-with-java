698466afda2687b740f2aa03267c15b4f4a4ebf0
locngohuynhtan <locngohuynhtan@gmail.com>
2025-06-13 18:05:51 +0700

    feat: introduce HelloJava project to get started with Java

---
+++ README.md
@@ -0,0 +1,5 @@
+﻿# Object-oriented Programming with Java
+
+### This repository contains small Java projects organized by sessions, each focusing on specific sections of object-oriented programming, progressing sequentially from beginner to advanced levels.
+
+#### © Copyright belongs to Loc Ngo-Huynh-Tan | Email: locngohuynhtan@gmail.com | [fb/locngohuynhtan](https://www.facebook.com/locngohuynhtan/) |
---
+++ Session01-Hello/HelloJava/src/hellojava/HelloJava.java
@@ -0,0 +1,15 @@
+package hellojava;
+
+public class HelloJava {
+
+    // Entry point, cửa chính, nơi cuộc chơi bắt đầu, CPU vào đây chạy lệnh.
+    public static void main(String[] args) {
+        System.out.println("Xin chào Java.\nWelcome to Java World.");
+        
+        System.out.println("5 + 5 = " + 5 + 5);
+        
+        System.out.println("5 + 5 = " + (5 + 5));
+        // sout tab
+    }
+    
+}
